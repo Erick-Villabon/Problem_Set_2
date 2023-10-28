d2d47bfea4c44d8bc7b5b547d7b78d50811eb2c5
Erick-Villabon <142061036+Erick-Villabon@users.noreply.github.com>
2023-10-28 01:37:08 -0500

    Update 2_pronosticos.R

--- scripts/2_pronosticos.R
+++ scripts/2_pronosticos.R
@@ -12,13 +12,13 @@ rm(list = ls())
 
 library(pacman)
 p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot, plotly, openxlsx, glmnet,
-       rio, leaflet, rgeos, tmaptools, sf, osmdata, tidymodels, writexl, 
+       rio, leaflet, rgeos, modeldata, vtable, tmaptools, sf, osmdata, tidymodels, writexl, 
        units, randomForest, rattle, spatialsample)
 
 # - Revisar el espacio de trabajo
 
-setwd("/Users/juandiego/Desktop/GitHub/Problem_Set_2/stores")
-#setwd("C:/Users/Erick/Desktop/Problem_Set_2/stores")
+#setwd("/Users/juandiego/Desktop/GitHub/Problem_Set_2/stores")
+setwd("C:/Users/Erick/Desktop/Problem_Set_2/stores")
 getwd()
 list.files()
 
@@ -54,14 +54,17 @@ db<- db %>% mutate(distancia_bus_2 =distancia_bus*distancia_bus)
 train<- train %>% mutate(distancia_bus_2 =distancia_bus*distancia_bus)
 test<- test %>% mutate(distancia_bus_2 =distancia_bus*distancia_bus)
 
-
-
-## Modelo Líneal
+##________________________________________________________________________
+#
+#                            Modelo Líneal
+#
+##________________________________________________________________________
 
 #Modelo 1
 
-rec_1 <- recipe(price ~ surface_total + bathrooms + bedrooms + property_type + distancia_universidades +
-                        distancia_bus  + distancia_policia + distancia_concesionarios + distancia_parque + estrato, data = db)%>% 
+rec_1 <- recipe(price ~ surface_total + bathrooms + bedrooms + property_type + 
+                  distancia_universidades + distancia_bus  + distancia_policia + 
+                  distancia_concesionarios + distancia_parque + estrato, data = db)%>% 
   step_dummy(all_nominal_predictors())
 
 reglineal<-linear_reg()
@@ -72,15 +75,12 @@ workf_1<-workflow() %>%
   add_model(reglineal)
 
 
-df_RMSE <- data.frame(Modelo = character(),RMSE = numeric())
-
-
 fit_1 <- workf_1 %>% 
   fit(data=train) 
 
 
-y1 <- predict(fit_1, new_data = train) %>% 
-  bind_cols(train) 
+y1 <- predict(fit_1, new_data = test) %>% 
+  bind_cols(test) 
 
 y1<- y1 %>%
   select(property_id, .pred)
@@ -96,21 +96,152 @@ write.table(y1, file = "ML_1.csv", sep = ",", row.names = FALSE, col.names = TRU
 
 
 
+##________________________________________________________________________
+#
+#                            Modelo Líneal
+#
+##________________________________________________________________________
 
 
+reg1 <- lm(price ~ surface_total + bathrooms + bedrooms + property_type + 
+                 distancia_universidades + distancia_bus  + distancia_policia + 
+                 distancia_concesionarios + distancia_parque + estrato, data = db)
+stargazer(reg1,type="text")
 
 
+# encontrar el lambda optimo
+  
+train_fold <- vfold_cv(train, v = 5)
+
+# Primera receta
+
+rec_1 <- recipe(price ~ surface_total + bathrooms + bedrooms + property_type + distancia_universidades +
+                  distancia_bus  + distancia_policia + distancia_concesionarios + distancia_parque + estrato , data = db) %>%
+  step_interact(terms = ~ estrato:bedrooms+bathrooms:property_type) %>% 
+  step_novel(all_nominal_predictors()) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_predictors()) %>% 
+  step_normalize(all_predictors())
 
 
+# Ridge
+ridge_spec <- linear_reg(penalty = tune(), mixture = 1) %>%
+  set_mode("regression") %>%
+  set_engine("glmnet")
 
+# Lasso
+lasso_spec <- linear_reg(penalty = tune(), mixture = 0) %>%
+  set_mode("regression") %>%
+  set_engine("glmnet")
 
+# Elastic Net
+elastic_net_spec <- linear_reg(penalty = tune(), mixture = .5) %>%
+  set_mode("regression") %>%
+  set_engine("glmnet")
 
+  # Workflows
+workflow_1.1 <- workflow() %>%
+  add_recipe(rec_1) %>%
+  add_model(ridge_spec)
 
+workflow_1.2 <- workflow() %>%
+  add_recipe(rec_1) %>%
+  add_model(lasso_spec)
 
+workflow_1.3 <- workflow() %>%
+  add_recipe(rec_1) %>%
+  add_model(elastic_net_spec)
 
+  # Penalización
 
+  penalty_grid <- grid_regular(penalty(range = c(-4, 4)), levels = 30)
+  penalty_grid
 
+  tune_res1 <- tune_grid(
+    workflow_1.1,         
+    resamples = train_fold,  
+    grid = penalty_grid,        
+    metrics = metric_set(rmse)
+  )
+  
+  tune_res2 <- tune_grid(
+    workflow_1.2,         
+    resamples = train_fold,  
+    grid = penalty_grid,        
+    metrics = metric_set(rmse)
+  )
 
+  tune_res3 <- tune_grid(
+    workflow_1.3,         
+    resamples = train_fold,  
+    grid = penalty_grid,        
+    metrics = metric_set(rmse)
+  )
+
+  # Seleccionar el mejor valor de penalización
+  best_penalty1 <- select_best(tune_res1, metric = "rmse")
+  best_penalty2 <- select_best(tune_res2, metric = "rmse")
+  best_penalty3 <- select_best(tune_res3, metric = "rmse")
+  
+  modelo_1.1 <- finalize_workflow(workflow_1.1, best_penalty1)
+  modelo_1.2 <- finalize_workflow(workflow_1.2, best_penalty2)
+  modelo_1.3 <- finalize_workflow(workflow_1.3, best_penalty3)
+  
+  # Entrenamos el primer modelo con los datos de train 
+  
+  fit_1.1 <- fit(modelo_1.1, data = train)
+  fit_1.2 <- fit(modelo_1.2, data = train)
+  fit_1.3 <- fit(modelo_1.3, data = train)
+  
+  # Evaluacipon del modelo
+  
+  augment(fit_1.1, new_data = train) %>%
+    mae(truth = price, estimate = .pred)
+  
+  augment(fit_1.2, new_data = train) %>%
+    mae(truth = price, estimate = .pred)
+  
+  augment(fit_1.3, new_data = train) %>%
+    mae(truth = price, estimate = .pred)
+
+  # Sacamos las predicciones sobre los datos de test 
+  
+  predictiones_1.1 <- predict(fit_1.1 , new_data = test)
+  
+  subida <- data.frame(
+    property_id = submission_template$property_id, 
+    .price = predictiones_1.1
+  )
+  colnames(subida)[2]<-"price"
+  
+  write.csv(subida,file='Ridge.csv', row.names=FALSE)
+  
+  
+  predictiones_1.2 <- predict(fit_1.2 , new_data = test)
+  
+  subida <- data.frame(
+    property_id = submission_template$property_id, 
+    .price = predictiones_1.2
+  )
+  colnames(subida)[2]<-"price"
+  
+  write.csv(subidafinal,file='Lasso.csv', row.names=FALSE)
+  
+  
+    
+  predictiones_1.3 <- predict(fit_1.3, new_data = test)
+  
+  subida <- data.frame(
+    property_id = submission_template$property_id, 
+    .price = predictiones_1.3
+  )
+  colnames(subida)[2]<-"price"
+
+  write.csv(subidafinal,file='Elastic_Net.csv', row.names=FALSE)
+  
+summary(predictiones_1.1)
+summary(predictiones_1.2)
+summary(predictiones_1.3)
 
 
 
